886b9da4c6c2dd41a2cf72fd7449da4c8471b424
Victor <rtsyssgy@gmail.com>
2024-07-22 10:20:14 +0900

    240722

--- HTML_CSS_JavaScript/Starbucks-homepage/js/main.js
+++ HTML_CSS_JavaScript/Starbucks-homepage/js/main.js
@@ -1,39 +1,53 @@
 console.log('starbucks main.js파일');
 
-const searchEl = document.querySelector('.search');
-const searchInputEl = document.querySelector('input');
+// const searchEl = document.querySelector('.search');
+// const searchInputEl = document.querySelector('input');
 
-// class가 search로 박스를 클릭했을 경우 이벤트 등록
-searchEl.addEventListener('click', function() {
-  // searchInputEl에서 포커스 이벤트 발생
-  searchInputEl.focus();
+// // class가 search로 박스를 클릭했을 경우 이벤트 등록
+// searchEl.addEventListener('click', function() {
+//   // searchInputEl에서 포커스 이벤트 발생
+//   searchInputEl.focus();
+// });
+
+// searchInputEl.addEventListener('focus',function(){
+//   searchInputEl.setAttribute('placeholder', '통합검색');
+// });
+
+// // 블러상태일 경우 글자 초기화
+// searchInputEl.addEventListener('blur',function(){
+//   searchInputEl.setAttribute('placeholder', '');
+// });
+
+// // const serchIcon = document.querySelector('header .inner .sub-menu .search span');
+// // serchBlurEl.addEventListener('blur', function(){
+// //   serchIcon.classList.add('blur');
+// // })
+
+// // tag가 input박스인 것에 focus 이벤트 등록
+// searchInputEl.addEventListener('focus', function() {
+//     searchEl.classList.add('focused');
+//     searchInputEl.setAttribute('placeholder', '통합검색');
+// });
+
+// // tag가 input박스인 것에 blur 이벤트 등록
+// searchInputEl.addEventListener('blur', function() {
+//     searchEl.classList.remove('focused');
+//     searchInputEl.setAttribute('placeholder', '');
+// }); 
+
+// jquery js-----------------------------------------
+// const searchEl = $('.search');
+// const searchInputEl = $('input');
+$(".search").on("click", function(event){
+  $('input').focus();
+});
+$('input').on("focus", function(event){
+  $('input').attr('placeholder', '통합검색')
+});
+$('input').on("blur", function(event){
+  $('input').attr('placeholder', '')
 });
 
-searchInputEl.addEventListener('focus',function(){
-  searchInputEl.setAttribute('placeholder', '통합검색');
-});
-
-// 블러상태일 경우 글자 초기화
-searchInputEl.addEventListener('blur',function(){
-  searchInputEl.setAttribute('placeholder', '');
-});
-
-// const serchIcon = document.querySelector('header .inner .sub-menu .search span');
-// serchBlurEl.addEventListener('blur', function(){
-//   serchIcon.classList.add('blur');
-// })
-
-// tag가 input박스인 것에 focus 이벤트 등록
-searchInputEl.addEventListener('focus', function() {
-    searchEl.classList.add('focused');
-    searchInputEl.setAttribute('placeholder', '통합검색');
-});
-
-// tag가 input박스인 것에 blur 이벤트 등록
-searchInputEl.addEventListener('blur', function() {
-    searchEl.classList.remove('focused');
-    searchInputEl.setAttribute('placeholder', '');
-}); 
 
 /*
   gsap사용법
@@ -42,16 +56,31 @@ searchInputEl.addEventListener('blur', function() {
 */
 
 const badgeEl = document.querySelector('header .badges');
+const selectToTop = document.querySelector('.to-top');
+// const selectToTop = document.querySelector('header .to-top');
+// const selectToTop2 = document.querySelector('header .to-top .material-symbols-outlined');
 window.addEventListener('scroll', _.throttle(() => {
   console.log('scrollY: ', window.scrollY);
 
   if(window.scrollY > 500){
     // badgeEl.style.display = 'none';
     gsap.to(badgeEl, 0.4, {opacity : 0, display: 'none'});
+    gsap.to(selectToTop,{duration: .2, x:0});
+    // sliding(selectToTop, -40, 10, 1);
   }else{
     gsap.to(badgeEl, 0.2, {opacity : 1,  display: 'block'});
+    gsap.to(selectToTop,{duration: .2, x:100});
+    // sliding(selectToTop, 10, -40, 1);
   }
 }, 500));
+
+selectToTop.addEventListener('click', function() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
+
+// function sliding(selector, fromX, toX, time){
+//   gsap.fromTo(selector, {y: fromX}, {duration: time, toX});
+// };
 
 const fadeEls = document.querySelectorAll('.visual1 .fade-in');
 fadeEls.forEach(function(fadeEl, index){
@@ -121,3 +150,33 @@ floatingObject('.floating3', 1.5, 20);
 function random(min, max) {
   return parseFloat((Math.random() * (max - min) + min).toFixed(2));
 }
+
+const awardsPromotion = new Swiper('.awards .swiper-container', {
+  // Optional parameters
+  // direction: 'horizeontal'
+  slidesPerView: 5,
+  spaceBetween: 30,
+  loop: true,
+  autoplay: true,
+  navigation: {
+    prevEl: '.awards .swiper-prev',
+    nextEl: '.awards .swiper-next'
+  },
+});
+
+const thisyear = document.querySelector('.this-year');
+thisyear.textContent = new Date().getFullYear();
+
+
+
+// const badgeEl = document.querySelector('header .badges');
+// window.addEventListener('scroll', _.throttle(() => {
+//   console.log('scrollY: ', window.scrollY);
+
+//   if(window.scrollY > 500){
+//     // badgeEl.style.display = 'none';
+//     gsap.to(badgeEl, 0.4, {opacity : 0, display: 'none'});
+//   }else{
+//     gsap.to(badgeEl, 0.2, {opacity : 1,  display: 'block'});
+//   }
+// }, 500));
